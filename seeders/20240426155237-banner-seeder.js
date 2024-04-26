893bd4cd6e0fd6f73ef999f0126f7cbc337e6cce
adityarizkyramadhan <adityarizky1020@gmail.com>
2024-04-26 23:06:52 +0700

    feat: add migration table banner and seeder

---
+++ seeders/20240426155237-banner-seeder.js
@@ -0,0 +1,45 @@
+'use strict';
+const {
+  Banner
+} = require('../models');
+const { v4: uuid } = require('uuid');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    const data = [
+      {
+        "id": uuid(),
+        "link": "https://is3.cloudhost.id/storage-foto/ciputra-banner/Item1.png"
+      },
+      {
+        "id": uuid(),
+        "link": "https://is3.cloudhost.id/storage-foto/ciputra-banner/Item2.png"
+      },
+      {
+        "id": uuid(),
+        "link": "https://is3.cloudhost.id/storage-foto/ciputra-banner/Item3.png"
+      }
+    ]
+    await Banner.bulkCreate(data)
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await Banner.destroy({ truncate: true })
+  }
+};
